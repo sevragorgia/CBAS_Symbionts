0553ba0938aad88b8d22d6643a043497c7355cc6
sevragorgia <sevragorgia@gmail.com>
2017-09-22 10:42:07 +0200

    finish blast script

--- Scripts/blast_fasta_against_genomes.sh
+++ Scripts/blast_fasta_against_genomes.sh
@@ -11,31 +11,25 @@ echo $CWD
 
 tar -xzf $FASTA_TARBALL
 
-for fasta in `ls *.fasta`; do
+for folder in `ls -d */`; do
 
-	echo "Processing $fasta"
-	
-	for folder in `ls -d */`; do
+	echo $folder
+	cd $folder
 
-		echo $folder
-		cd $folder
-		DB=`ls *.faa.gz`#need to change this to match what is in the genome folders
-		blastp -query $fasta -db ${DB%.faa.gz} -outfmt 6 -max_target $3 --output #add output to match the query name
-		cd ..
+	DB=`ls *.faa.gz`
+
+	for fasta in `ls ../*.fasta`; do
+
+		echo "Processing $fasta"
+		
+		OUTPUT_NAME=`basename $fasta .fasta`
+		echo blastp -query $fasta -db ${DB%.*} -outfmt 6 -max_target_seqs $3 -evalue $4 -out $OUTPUT_NAME
+		blastp -query $fasta -db ${DB%.*} -outfmt 6 -max_target_seqs $3 -evalue $4 -out $OUTPUT_NAME 
 	done
+
+	cd ..
 
 done
 
 
-#for folder in `ls -d */`; do
-
-#	query=`ls $PWD/*.fasta`
-#echo $folder
-#echo $query
-#cd $folder
-#DB=`ls *.faa`
-#blastp -query $query -db $DB -outfmt 6 -max_target_seqs 10
-#cd ..
-
-#done<$1 
 
